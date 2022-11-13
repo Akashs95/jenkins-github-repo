1414760c681b6e900eae69626bade2eacbe64066
Akashs95 <sonawaneaakash56@gmail.com>
2022-11-13 12:36:32 +0530

    new line added

--- Jenkinsfiles/sample_jenkins_file.sh
+++ Jenkinsfiles/sample_jenkins_file.sh
@@ -2,3 +2,4 @@
 echo "this is practice file created"
 echo "this is simple command to check webhook"
 echo " running this line after webhook sethub has done"
+echo "be careful while adding an url in webhook"
